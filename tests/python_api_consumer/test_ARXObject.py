42379bbc8638d2d55c8175391c4acde66dc2bcd2
sondre <sondre.hal@gmail.com>
2019-01-22 16:06:47 +0100

    Add more tests to ARXObject to comply with test coverage req

--- tests/python_api_consumer/test_ARXObject.py
+++ tests/python_api_consumer/test_ARXObject.py
@@ -3,5 +3,17 @@ from python_api_consumer.ARX_object import ARXObject
 
 class ARXObjectTest(unittest.TestCase):
 
+    def setUp(self):
+        self.test_object = ARXObject("tom url")
+
+
     def test_init__sanity_test(self):
         obj = ARXObject("tom url")
+
+
+    def test_add_data__sanity_test(self):
+        self.test_object.add_data("test_data")
+
+    def test_add_config__sanity_test(self):
+        self.test_object.add_config("test_config")
+
