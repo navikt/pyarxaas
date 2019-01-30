82dfd3135d25d85b12808a89727a3f888e06e3ed
sondre <sondre.hal@gmail.com>
2019-01-30 23:40:50 +0100

    Create draft functionality of AaaS class

--- python_api_consumer/aaas.py
+++ python_api_consumer/aaas.py
@@ -1,18 +1,34 @@
+import sys
+import os
+from collections.abc import MutableMapping
 
+from python_api_consumer.aaas_connector import AaaSConnector
+from python_api_consumer.models.anonymize_payload import AnonymizePayload
 
 class AaaS:
     """ ARX Web Service Connector object for connecting to the Web API.
         Add data to be anonymized, and configure anonymizaton
      """
 
-    def __init__(self, url:str):
-        self._endpoint_url = url
+    def __init__(self, url: str=None):
+
+        if url is None:
+            url = self._get_url_from_env()
+
+        self.conn = AaaSConnector(base_url=url)
+
+        self._data = {}
 
     def add_data(self, data):
         self._data = data
 
-    def add_config(self, config):
-        self._config = config
+    def anonymize(self) -> MutableMapping:
+        return self.conn.anonymize_data(self._data)
 
 
-
+    @staticmethod
+    def _get_url_from_env():
+        try:
+            return os.environ["AAAS_URL"]
+        except KeyError:
+            raise EnvironmentError("No AAAS_URL set in the environment")
--- python_api_consumer/aaas_connector.py
+++ python_api_consumer/aaas_connector.py
@@ -1,6 +1,5 @@
 from uplink import Consumer, get, headers, Path, Query, post, Body, json
 
-from python_api_consumer.models.anonymize_payload import AnonymizePayload
 
 
 class AaaSConnector(Consumer):
