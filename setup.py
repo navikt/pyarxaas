9eae78747f30c468969a114dfd4731491ff0c6dd
sondre <sondre.hal@gmail.com>
2019-01-31 19:06:52 +0100

    Fix dependency version bug

--- setup.py
+++ setup.py
@@ -86,7 +86,8 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['uplink'],  # Optional
+    install_requires=["uplink==0.7.0",
+                      "pandas"],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
