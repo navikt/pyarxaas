102ab44d813d7ec91d96436c5c36ddee8c99ef27
Julian <teritry2@gmail.com>
2019-02-14 15:41:49 +0100

    Moving all the Ldiversity related tests into the ldiversity test file

--- tests/pyaaas/models/test_privacy_models_ldiversity.py
+++ tests/pyaaas/models/test_privacy_models_ldiversity.py
@@ -3,6 +3,21 @@ import unittest
 
 
 class PrivacyModelsLDiversityTest(unittest.TestCase):
+
+    def test_privacyModelIterator(self):
+        self.privacymodel = LDiversityDistinct(l=3, column_name="column")
+        self.assertEqual(list(iter(self.privacymodel))[1], "column_name", "Iterator does not create correct list of values")
+
+
+    def test_lenght(self):
+        self.privacymodel = LDiversityDistinct(l=3, column_name="column")
+        self.assertEqual(len(self.privacymodel), 2)
+
+
+    def test_message(self):
+        self.privacymodel = LDiversityDistinct(l=3, column_name="column")
+        self.assertEqual(str(self.privacymodel), "LDiversityDistinct(l=3, column_name=column)")
+
     def test_set_data_distinct(self):
         self.distict = LDiversityDistinct(l=3, column_name="sensitive_column_name")
         self.assertEqual(self.distict._anonymity_name, "LDIVERSITY_DISTINCT", "Recursive model name not correct")
