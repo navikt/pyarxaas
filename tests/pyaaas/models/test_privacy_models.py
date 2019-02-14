018a78087fa2eed0798133688ee0e5cd722afd7a
Julian <teritry2@gmail.com>
2019-02-14 15:01:18 +0100

    Fixing indentation error from previous commit

--- tests/pyaaas/models/test_privacy_models.py
+++ tests/pyaaas/models/test_privacy_models.py
@@ -3,17 +3,17 @@ import unittest
 
 class PrivacyModelsTest(unittest.TestCase):
 
-def test_privacyModelIterator(self):
-    self.privacymodel = LDiversityDistinct(l=3, column_name="column")
-    self.assertEqual(list(iter(self.privacymodel))[1], "column_name", "Iterator does not create correct list of values")
+    def test_privacyModelIterator(self):
+        self.privacymodel = LDiversityDistinct(l=3, column_name="column")
+        self.assertEqual(list(iter(self.privacymodel))[1], "column_name", "Iterator does not create correct list of values")
 
 
-def test_lenght(self):
-    self.privacymodel = LDiversityDistinct(l=3, column_name="column")
-    self.assertEqual(len(self.privacymodel), 2)
+    def test_lenght(self):
+        self.privacymodel = LDiversityDistinct(l=3, column_name="column")
+        self.assertEqual(len(self.privacymodel), 2)
 
 
-def test_message(self):
-    self.privacymodel = LDiversityDistinct(l=3, column_name="column")
-    self.assertEqual(str(self.privacymodel), "LDiversityDistinct(l=3, column_name=column)")
+    def test_message(self):
+        self.privacymodel = LDiversityDistinct(l=3, column_name="column")
+        self.assertEqual(str(self.privacymodel), "LDiversityDistinct(l=3, column_name=column)")
 
