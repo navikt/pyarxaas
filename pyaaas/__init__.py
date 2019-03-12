9d28fa87ef3ee229e69eaa784640ccc0e425209a
andregroseth <andregroseth@hotmail.com>
2019-03-12 12:44:25 +0100

    CREATE new AaaS class to represent the connection to the AaaS backend.
    CREATE Dataset class to represent the data to be worked on.

---
+++ pyaaas/__init__.py
@@ -0,0 +1,2 @@
+from pyaaas.models.privacy_models import KAnonymity
+__all__ = ['KAnonymity']
---
+++ tests/pyaaas/test_Dataset.py
@@ -0,0 +1,17 @@
+import unittest
+
+from pyaaas.attribute_type import AttributeType
+
+
+class DatasetTest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_data = [['id', 'name'],
+                         ['0', 'Viktor'],
+                         ['1', 'Jerry']]
+        self.test_attribute_type_mapping = {'id': AttributeType.IDENTIFYING,
+                                            'name': AttributeType.QUASIIDENTIFYING}
+
+    def test_init(self):
+        Dataset(self.test_data, self.test_attribute_type_mapping)
+
