50164278e5332fd2d7c41f8ae7545c6cd10edf79
sondre <sondre.hal@gmail.com>
2019-03-25 15:06:06 +0100

    Handle non 2xx responses form service

--- pyaaas/aaas.py
+++ pyaaas/aaas.py
@@ -1,5 +1,7 @@
 import json
 
+import requests
+
 from pyaaas.aaas_connector import AaaSConnector
 from pyaaas.dataset import Dataset
 from pyaaas.risk_profile import RiskProfile
@@ -20,6 +22,7 @@ class AaaS:
             models.append(model._payload())
         data_dict["privacyModels"] = models
         response = self._connector.anonymize_data(data_dict)
+        self._throw_exeption_on_error_response(response)
         json_string = response.text
         response_dict = json.loads(json_string)
         return Dataset(response_dict["anonymizeResult"]["data"])
@@ -27,5 +30,12 @@ class AaaS:
     def risk_profile(self, dataset: Dataset):
         data_dict = dataset._payload()
         response = self._connector.risk_profile(data_dict)
+        self._throw_exeption_on_error_response(response)
         metric_dict = json.loads(response.text)
         return RiskProfile(metric_dict)
+
+    @staticmethod
+    def _throw_exeption_on_error_response(response: requests.Response) -> None:
+        # if status code does not start with 2xx throw exception
+        if not str(response.status_code)[0] == "2":
+            raise SystemError(response.text)
