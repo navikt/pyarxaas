32e724d1f4aba7e6a93595eb2ad6d138f7900684
Sondre <sondre.halvorsen@nav.no>
2019-03-29 18:02:51 +0100

    Create data_generator for generating test data

---
+++ tests/pyaaas/data_generator.py
@@ -0,0 +1,118 @@
+# module for collection of test data generating functions and classes
+from pyaaas.anonymize_result import AnonymizeResult
+from pyaaas.attribute_type import AttributeType
+from pyaaas.dataset import Dataset
+from pyaaas.risk_profile import RiskProfile
+
+
+def id_name_dataset() -> Dataset:
+    test_data = [['id', 'name'],
+                      ['0', 'Viktor'],
+                      ['1', 'Jerry']]
+    test_attribute_type_mapping = {'id': AttributeType.IDENTIFYING,
+                                        'name': AttributeType.QUASIIDENTIFYING}
+    return Dataset(test_data, test_attribute_type_mapping)
+
+
+def anonymization_metrics():
+    metrics = {"anonymizationStatus": "ANONYMIZED"}
+    return AnonymizeResult.AnonymizationMetrics(metrics)
+
+
+def risk_profile() -> RiskProfile:
+    test_metric = {"reIdentificationRisk": {
+        "measure_value": "[%]",
+        "Prosecutor_attacker_success_rate": "98.72",
+        "records_affected_by_highest_prosecutor_risk": "97.46000000000001",
+        "sample_uniques": "97.46000000000001",
+        "estimated_prosecutor_risk": "100.0",
+        "population_model": "PITMAN",
+        "highest_journalist_risk": "100.0",
+        "records_affected_by_lowest_risk": "0.06",
+        "estimated_marketer_risk": "98.72000000000001",
+        "Journalist_attacker_success_rate": "98.72",
+        "highest_prosecutor_risk": "100.0",
+        "estimated_journalist_risk": "100.0",
+        "lowest_risk": "33.33333333333333",
+        "Marketer_attacker_success_rate": "98.72",
+        "average_prosecutor_risk": "98.72000000000001",
+        "records_affected_by_highest_journalist_risk": "97.46000000000001",
+        "population_uniques": "39.64593493418713",
+        "quasi_identifiers": ["Innvandrerbakgrunn", "Ytelse", "Innsatsgruppe", "Ledighetsstatus"]
+    },
+        "distributionOfRisk": {"riskIntervalList": [{"interval": "]50,100]",
+                                                     "recordsWithRiskWithinInteval": 0.9746,
+                                                     "recordsWithMaxmalRiskWithinInterval": 1.0},
+                                                    {"interval": "]33.4,50]",
+                                                     "recordsWithRiskWithinInteval": 0.0248,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0254},
+                                                    {"interval": "]25,33.4]",
+                                                     "recordsWithRiskWithinInteval": 0.0006,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0006},
+                                                    {"interval": "]20,25]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]16.7,20]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]14.3,16.7]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]12.5,14.3]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]10,12.5]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]9,10]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]8,9]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]7,8]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]6,7]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]5,6]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]4,5]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]3,4]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]2,3]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]1,2]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]0.1,1]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]0.01,0.1]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]0.001,0.01]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]0.0001,0.001]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]1e-5,0.0001]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]1e-6,1e-5]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0},
+                                                    {"interval": "]0,1e-6]",
+                                                     "recordsWithRiskWithinInteval": 0.0,
+                                                     "recordsWithMaxmalRiskWithinInterval": 0.0}]
+                               }
+
+    }
+    return RiskProfile(test_metric)
