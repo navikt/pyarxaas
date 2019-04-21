0b408944d99b6bf7c01460658c5b178ff2f4f6ea
sondre <sondre.hal@gmail.com>
2019-04-21 14:19:20 +0200

    Fix hierarchy builders importing

--- pyaaas/models/hierarchy/__init__.py
+++ pyaaas/models/hierarchy/__init__.py
@@ -2,4 +2,4 @@ from pyaaas.models.hierarchy.interval_builder.interval_hierarchy_builder import 
 from pyaaas.models.hierarchy.reduction_hierarchy_builder import RedactionHierarchyBuilder
 from pyaaas.models.hierarchy.order_builder.order_hierarchy_builder import OrderHierarchyBuilder
 
-__all__ = ['IntervalHierarchyBuilder', 'RedactionHierarchyBuilder']
+__all__ = ['IntervalHierarchyBuilder', 'RedactionHierarchyBuilder', 'OrderHierarchyBuilder']
